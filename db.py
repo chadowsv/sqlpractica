2674d0d9f49278a07ba910509bfb8ede0b702789
chadowsv <lisethsbv2013@gmail.com>
2026-04-01 22:05:16 -0500

    practica

--- db.py
+++ db.py
@@ -13,8 +13,10 @@ engine = create_engine(DATABASE_URL,echo=True)
 
 def create_all_tables(app: FastAPI):
     SQLModel.metadata.create_all(engine)
+    #Pasa y luego continua con la ejecución de la aplicación
     yield
 def get_session():
     with Session(engine) as session:
         yield session
+#Depends significa que se va a encargar automaticamente de crear una sesión y pasarla a la función que lo requiera
 SessionDep = Annotated[Session, Depends(get_session)]
